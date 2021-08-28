fe623539dee059ab721659ada0e035293046165a
Wisterleys <wister.sousa2@gmail.com>
2021-08-28 12:52:59 -0300

    melhorias para overload

--- js/index.js
+++ js/index.js
@@ -5,9 +5,11 @@ const CONFIG ={
     x:50,
     y:0
 }
+let val = true
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
+
 setInterval(function(){
-    new Bomb(CONFIG.place,getRandom(0, 100),CONFIG.y,1000,"black")
+    document.hasFocus()?new Bomb(CONFIG.place,getRandom(0, 100),CONFIG.y,1000,"black"):0
 },1000)
